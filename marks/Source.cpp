025a5c99f9cf5420d99b09af50d0ed1fa40fdf95
Pavel <cherkasov08pavel@gmail.com>
2025-10-17 22:57:19 +0300

    f

--- marks/Source.cpp
+++ marks/Source.cpp
@@ -3,9 +3,37 @@ using namespace std;
 
 class AcademicPerformance
 {
-	int size;
+	int size = 10;
 	double* markArr;
-	int newMark;
-	double average;
+public:
+	AcademicPerformance()
+	{
+		markArr = nullptr;
+	}
 
+	void Programm()
+	{
+		int average;
+		int newMark;
+		int choise;
+		cout << "Enter the operation: " << endl;
+		cout << "1 - Enter the marks" << endl;
+		cout << "2 - Show your marks" << endl;
+		cout << "3 - Rewrite marks" << endl;
+		cout << "4 - Show if you have a scholarship" << endl;
+		cout << "5 - Sort your array" << endl;
+		cin >> choise;
+
+		switch (choise)
+		{
+
+		default:
+			break;
+		}
+	}
 };
+
+int main()
+{
+
+}
